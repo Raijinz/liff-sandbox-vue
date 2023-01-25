c556c4193e9c1d859db78cf421291d8640cc6962
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2023-01-25 17:59:16 +0700

    Add testing for input image file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+// import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    <input type="file" accept="image/*" />
   </main>
 </template>
