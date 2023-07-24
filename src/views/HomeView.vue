61d09cbbfe7359307b908f4c43e1ffcd67e74721
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2023-07-24 10:29:43 +0700

    Test immediate redirection

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import type { Ref } from "vue";
+/* import { ref } from "vue";
+import type { Ref } from "vue"; */
 import liff from "@line/liff";
 
-const os = ref(liff.getOS());
+/* const os = ref(liff.getOS());
 const imageFiles: Ref<FileList | null | undefined> = ref();
 const imageFile: Ref<File | null | undefined> = ref();
 const previewFiles = (event: Event) => {
   const target = event.target as HTMLInputElement;
   imageFile.value = target.files?.[0] || null;
   imageFiles.value = target.files;
-};
+}; */
 const redirect = () => {
-  location.replace("https://www.google.co.th");
+  // location.replace("https://www.google.co.th");
+  liff.openWindow({
+    url: "https://www.google.co.th",
+  });
 };
 // import TheWelcome from "@/components/TheWelcome.vue";
+redirect();
 </script>
 
 <template>
   <main>
     <!-- <TheWelcome /> -->
-    <input
+    <!-- <input
       v-if="os !== 'android'"
       id="file"
       type="file"
@@ -40,6 +44,6 @@ const redirect = () => {
     <li v-for="n in imageFiles?.length" :key="n">
       {{ imageFiles?.item(n - 1)?.name }}
     </li>
-    <button type="button" @click="redirect">Go to Google (TH)</button>
+    <button type="button" @click="redirect">Go to Google (TH)</button> -->
   </main>
 </template>
