1ab4306a0984ce68e2812c84c95ace0e3959ecf1
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2023-10-05 12:38:28 +0700

    Add OA messaging

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 /* import { ref } from "vue";
 import type { Ref } from "vue"; */
-import liff from "@line/liff";
+// import liff from "@line/liff";
 
 /* const os = ref(liff.getOS());
 const imageFiles: Ref<FileList | null | undefined> = ref();
@@ -11,19 +11,27 @@ const previewFiles = (event: Event) => {
   imageFile.value = target.files?.[0] || null;
   imageFiles.value = target.files;
 }; */
-const redirect = () => {
+/* const redirect = () => {
   // location.replace("https://www.google.co.th");
   liff.openWindow({
     url: "https://www.muangthai.co.th/th/vdo-call?utm_source=website&utm_medium=ow-aw-mtlwebsite--VideoCall-SmartLinkPro101Global-pid10183-20230503&utm_campaign=alnnnncc67",
   });
-};
+}; */
 // import TheWelcome from "@/components/TheWelcome.vue";
 // redirect();
+function redirectToOA() {
+  location.replace(
+    `https://line.me/R/oaMessage/${encodeURIComponent(
+      "@Kbanklive"
+    )}/${encodeURIComponent("@activate")}`
+  );
+}
 </script>
 
 <template>
   <main>
-    <a href="tel:+66810413477">Call me</a>
+    <button type="button" @click="redirectToOA">To @activate KBank Live</button>
+    <!-- <a href="tel:+66810413477">Call me</a> -->
     <!-- <TheWelcome /> -->
     <!-- <input
       v-if="os !== 'android'"
