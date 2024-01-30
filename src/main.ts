de5035ab0f869e46812b0dc8bc1d96c30070804b
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2024-01-30 14:30:23 +0700

    Finally remove vConsole

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import VConsole from "vconsole";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
-new VConsole();
 
 app.use(createPinia());
 app.use(router);
